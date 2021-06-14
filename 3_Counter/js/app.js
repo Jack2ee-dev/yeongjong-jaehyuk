425889fc78587fb0317696cec8032864dcf9fbc7
Jack2ee-dev <Jack2ee.dev@gmail.com>
2021-06-14 16:07:41 +0900

    feat: Implement 3_Counter

---
+++ 3_Counter/js/app.js
@@ -0,0 +1,44 @@
+// constants
+const COUNTER_DEFAULT_VALUE = 0;
+
+// elements
+const $counter = document.querySelector('.counter');
+const $increaseBtn = document.querySelector('.increase');
+const $decreaseBtn = document.querySelector('.decrease');
+
+// hooks
+const useState = defaultValue => {
+  let value = defaultValue;
+
+  const get = () => value;
+  const set = updatedValue => {
+    value = updatedValue;
+    render();
+  };
+
+  return [get, set];
+};
+
+// states
+const [counterValue, setCounterValue] = useState(COUNTER_DEFAULT_VALUE);
+
+// functions
+const increase = () => {
+  setCounterValue(counterValue() + 1);
+};
+
+const decrease = () => {
+  if (+$counter.textContent <= 0) return;
+  setCounterValue(counterValue() - 1);
+};
+
+// utils
+function render() {
+  $counter.textContent = counterValue();
+}
+
+document.addEventListener('DOMContentLoaded', render);
+
+$increaseBtn.addEventListener('click', increase);
+
+$decreaseBtn.addEventListener('click', decrease);
