0257c00a59601466d10f186bfbf724356e2eaec4
yeongjong310 <tohero21@naver.com>
2021-06-14 17:33:28 +0900

    feat: Implement 4_Analog-clock

---
+++ 4_Analog-clock/js/app.js
@@ -0,0 +1,18 @@
+const $secondHand = document.querySelector('.clock > .hand.second');
+const $minuteHand = document.querySelector('.clock > .hand.minute');
+const $hourHand = document.querySelector('.clock > .hand.hour');
+
+const render = () => {
+  const now = new Date();
+
+  const second = now.getSeconds();
+  const minute = now.getMinutes();
+  const hour = now.getHours();
+
+  $secondHand.style.setProperty('--deg',  second * 6);
+  $minuteHand.style.setProperty('--deg', minute * 6 + second * 0.1);
+  $hourHand.style.setProperty('--deg', hour * 30 + minute * 0.5 + second * (1 / 120));
+}
+
+window.addEventListener('DOMContentLoaded', () => (setInterval(render , 1000)));
+
