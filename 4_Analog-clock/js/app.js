76c33b25a8342c747d8be7b285785a62dd3e8e4b
Jack2ee-dev <Jack2ee.dev@gmail.com>
2021-06-15 11:03:37 +0900

    refactor: Apply prettier in 4_Analog-clock

--- 4_Analog-clock/js/app.js
+++ 4_Analog-clock/js/app.js
@@ -9,10 +9,12 @@ const render = () => {
   const minute = now.getMinutes();
   const hour = now.getHours();
 
-  $secondHand.style.setProperty('--deg',  second * 6);
+  $secondHand.style.setProperty('--deg', second * 6);
   $minuteHand.style.setProperty('--deg', minute * 6 + second * 0.1);
-  $hourHand.style.setProperty('--deg', hour * 30 + minute * 0.5 + second * (1 / 120));
-}
+  $hourHand.style.setProperty(
+    '--deg',
+    hour * 30 + minute * 0.5 + second * (1 / 120)
+  );
+};
 
-window.addEventListener('DOMContentLoaded', () => (setInterval(render , 1000)));
-
+window.addEventListener('DOMContentLoaded', () => setInterval(render, 1000));
