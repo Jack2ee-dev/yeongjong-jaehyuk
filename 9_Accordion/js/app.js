c19fc3540dfcc470649779beaee3c1e5d8623157
Jack2ee-dev <Jack2ee.dev@gmail.com>
2021-06-16 15:43:28 +0900

    fix: Add transitioned event handler

--- 9_Accordion/js/app.js
+++ 9_Accordion/js/app.js
@@ -1,18 +1,30 @@
 const $accordion = document.querySelector('.accordion');
+const $submenu = document.querySelector('.submenu');
+const $activeMenu = document.querySelector(
+  '.accordion > .menu-container.active'
+);
+const $menuContainers = document.getElementsByClassName('menu-container');
 
 // event handlers
 $accordion.addEventListener('click', e => {
   if (!e.target.matches('.accordion > .menu-container > .menu')) return;
-  [...document.getElementsByClassName('menu-container')].forEach(
-    $menuContainer => {
-      $menuContainer.classList.toggle(
-        'active',
-        $menuContainer === e.target.parentNode
-      );
-      $menuContainer.querySelector('.submenu').style.height =
-        $menuContainer === e.target.parentNode
-          ? e.target.nextElementSibling.scrollHeight + 'px'
-          : 0;
-    }
-  );
+  [...$menuContainers].forEach($menuContainer => {
+    $menuContainer.classList.toggle(
+      'active',
+      $menuContainer === e.target.parentNode
+    );
+    $menuContainer.querySelector('.submenu').style.height =
+      $menuContainer === e.target.parentNode
+        ? e.target.nextElementSibling.scrollHeight + 'px'
+        : 0;
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  $activeMenu.querySelector('.submenu').style.height =
+    $submenu.scrollHeight + 'px';
+});
+
+$submenu.addEventListener('transitionend', () => {
+  $accordion.style.opacity = 1;
 });
